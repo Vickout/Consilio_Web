11c243d0ae22aa6b789a4bbbf8f37bc30934558a
Guilherme Lopes <guilherme@netsoft.eti.br>
2020-05-04 14:35:39 -0300

    ajustes rota listaExecucao

--- src/pages/ListaExecucao/index.js
+++ src/pages/ListaExecucao/index.js
@@ -4,7 +4,8 @@ import data from "./data.json";
 import "./styles.css";
 import Board from "react-trello";
 
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Table, Tag, Switch } from 'antd';
+
 import {
     BarsOutlined,
     BarChartOutlined,
@@ -13,6 +14,7 @@ import {
     ScheduleOutlined,
     SearchOutlined,
     TeamOutlined,  
+    PlusCircleOutlined
   } from '@ant-design/icons';
 import './styles.css';
 
@@ -20,29 +22,95 @@ import logo from "../../assets/logo_branco.png"
 
 const { Content, Sider } = Layout;
 
-function UncontrolledBoard() {
-    return (
-      <Board
-        allowRemoveLane
-        allowRenameColumn
-        allowRemoveCard
-        onLaneRemove={console.log}
-        onCardRemove={console.log}
-        onLaneRename={console.log}
-        initialBoard={data}
-        allowAddCard={{ on: "top" }}
-        onNewCardConfirm={draftCard => ({
-          id: new Date().getTime(),
-          ...draftCard
-        })}
-        onCardNew={console.log}
-      />
-    );
-  }
-
 export default function ListaExecucao() {
-    
+
     const [state, setState] = useState(false);
+
+    const [fluxo, setFluxo] = useState(true);
+    const [tipoCheck, setTipoCheck] = useState('Fluxo');
+
+    function onChange(checked) {
+      console.log(`switch to ${checked}`);
+      
+      if(checked) {
+        setTipoCheck('Fluxo')
+      } else  {
+        setTipoCheck('Lista')
+      }
+      setFluxo(checked);
+    }
+
+    const columns = [
+      {
+        title: 'CUT',
+        dataIndex: 'cut',
+        key: 'cut',
+        render: text => <a>{text}</a>,
+      },
+      {
+        title: 'Tarefa',
+        dataIndex: 'tarefa',
+        key: 'tarefa',
+      },
+      {
+        title: 'Status',
+        key: 'status',
+        dataIndex: 'status',
+        render: tags => (
+          <span>
+            {tags.map(tag => {
+              let color = tag.length > 5 ? 'geekblue' : 'green';
+              if (tag === 'loser') {
+                color = 'volcano';
+              }
+              return (
+                <Tag color={color} key={tag}>
+                  {tag.toUpperCase()}
+                </Tag>
+              );
+            })}
+          </span>
+        ),
+      },
+      {
+        title: 'Responsável',
+        dataIndex: 'responsavel',
+        key: 'responsavel',
+      },
+      {
+        title: 'Detalhes',
+        key: 'action',
+        render: (text, record) => (
+          <span>
+            <PlusCircleOutlined />
+          </span>
+        ),
+      },
+    ];
+    
+    const dataTabela = [
+      {
+        key: '1',
+        cut: '100',
+        tarefa: 'Padrão de atendimento',
+        status: ['A fazer'],
+        responsavel: 'Gestor comercial'
+      },
+      {
+        key: '2',
+        cut: '125',
+        tarefa: 'Funil de vendas',
+        status: ['A fazer'],
+        responsavel: 'Gestor comercial'
+      },
+      {
+        key: '3',
+        cut: '80',
+        tarefa: 'Agendamentos',
+        status: ['A fazer'],
+        responsavel: 'Gestor comercial'
+      },
+    ];
 
     return (
         <div className="container">
@@ -65,8 +133,15 @@ export default function ListaExecucao() {
                         <img src={logo} alt="consilio"/>
                     </header>
                     <Content style={{ margin: '30px 30px' }}>
+                        <div style={{ textAlign: 'right' }}>{tipoCheck}<br></br>
+                          <Switch defaultChecked checked={fluxo} onChange={onChange} />
+                        </div>
                         <div className="App">
+                          { fluxo ?
                             <Board data={data} laneDraggable canAddLanes />
+                            :
+                            <Table columns={columns} dataSource={dataTabela} />
+                          }
                         </div>
                     </Content>
                 </Layout>
